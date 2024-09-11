3f9060492345abe48898d8c22dd5614fa0e666d6
주재현 <jaehyun.joo@yplabs.kr>
2024-09-11 12:48:55 +0900

    test 연산자 추가

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -13,6 +13,8 @@ func TestNextToken(t *testing.T) {
 			x + y;
 		};
 		let result = add(five, ten);
+		!-/*5;
+		5 < 10 > 5;
 	`
 
 	tests := []struct {
@@ -55,6 +57,18 @@ func TestNextToken(t *testing.T) {
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"}, 
 		{token.EOF, ""},
 	}
 
