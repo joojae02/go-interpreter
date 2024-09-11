ef76128ea2ce06f7e1a2010b6bbc10a85117e2ab
주재현 <jaehyun.joo@yplabs.kr>
2024-09-11 12:56:18 +0900

    ==, != 비교 연산자 추가

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -43,9 +43,21 @@ func (l *Lexer) NextToken() token.Token {
 				tok = newToken(token.ILLEGAL, l.ch)
 			}
 		case '=':
-			tok = newToken(token.ASSIGN, l.ch)
+			if l.peekChar() == '=' {
+				ch := l.ch
+				l.readChar()
+				tok = token.Token{Type: token.EQ, Literal: string(ch) + string(l.ch)}
+			} else {
+				tok = newToken(token.ASSIGN, l.ch)
+			}
 		case '!':
-			tok = newToken(token.BANG, l.ch)
+			if l.peekChar() == '=' {
+				ch := l.ch
+				l.readChar()
+				tok = token.Token{Type: token.NOT_EQ, Literal: string(ch) + string(l.ch)}
+			} else {
+				tok = newToken(token.BANG, l.ch)
+			}
 		case '+':
 			tok = newToken(token.PLUS, l.ch)
 		case '-':
@@ -111,4 +123,12 @@ func (l *Lexer) readNumber() string {
 
 func isDigit(ch byte) bool {
 	return '0' <= ch && ch <= '9'
+}
+
+func (l *Lexer) peekChar() byte {
+	if l.readPostion >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPostion]
+	}
 }
